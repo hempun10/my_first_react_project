c75d5b9748c28bb63ea3f896a35e770656adcdeb
hem <hemdrpun10@gmail.com>
2022-12-20 22:48:22 +0545

    Adding Dymanic Search With onCLick and KeyPress

--- src/App.js
+++ src/App.js
@@ -11,15 +11,16 @@ const API_URL = "https://www.omdbapi.com?apikey=7b225a20";
 const App = () => {
   //Getting movies from API
   const [movies,setMovies] = useState([]);
-
+  // Seeting Up Dynamic Search State 
+  const [searchTerm, setSearchTerm] = useState('');
 
   const searchMovies = async (title) => {
-    const response = await fetch(`${API_URL}&s=${title}`);
+    const response = await fetch(`${API_URL}&s=${title}`); //Feeting response from api and Storing as variable
     const data = await response.json();
-    setMovies(data.Search)
+    setMovies(data.Search) //Assigning received movies in data variable and passing dyanamic values tosetMovie state
   };
   useEffect(() => {
-    searchMovies("Avatar");
+    searchMovies("Avatar"); //Show the movies on reload or as first deafult home
   }, []);
 
   return (
@@ -29,10 +30,12 @@ const App = () => {
       <div className="search">
         <input
           placeholder="Search your fav movies !"
-          value="SuperMan"
-          onChange={() => {}}
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value) } //Getting input from user and passing value to search state
+          onKeyUp={(e)=>e.key==="Enter"?searchMovies(searchTerm):null} //Searching movies while pressign enter key
         />
-        <img src={SearchIcon} alt="search-icon" onClick={() => {}} />
+        <img src={SearchIcon} alt="search-icon"  onClick={() => searchMovies(searchTerm)} //Searching movies while search key is clicked
+         />
       </div>
 
      { movies?.length > 0 
