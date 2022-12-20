ca9e2c72c5fd6f5b74e10b370072bfaadef815bf
hem <hemdrpun10@gmail.com>
2022-12-20 22:26:36 +0545

    Adding Dynamic Movies using Map and Passing props to an Movie Card Component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from "react";
-import { useEffect } from "react";
+import { useEffect,useState } from "react";
 import "./App.css";
 import MovieCard from "./components/MovieCard";
 import SearchIcon from "./search.svg";
 
 // 7b225a20
 const API_URL = "https://www.omdbapi.com?apikey=7b225a20";
+
+
 const App = () => {
-  const movie1 = {
-    Title: "Avatar: The Last Airbender",
-    Year: "2005–2008",
-    imdbID: "tt0417299",
-    Type: "series",
-    Poster: "https://m.media-amazon.com/images/M/MV5BODc5YTBhMTItMjhkNi00ZTIxLWI0YjAtNTZmOTY0YjRlZGQ0XkEyXkFqcGdeQXVyODUwNjEzMzg@._V1_SX300.jpg",
-  };
+  //Getting movies from API
+  const [movies,setMovies] = useState([]);
+
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
-    console.log(data.Search);
+    setMovies(data.Search)
   };
   useEffect(() => {
     searchMovies("Avatar");
@@ -36,9 +34,20 @@ const App = () => {
         />
         <img src={SearchIcon} alt="search-icon" onClick={() => {}} />
       </div>
-      <div className="container">
-       <MovieCard movie1={movie1}/>
+
+     { movies?.length > 0 
+      ? ( <div className="container">
+      {/* Selecting Movies element using map function  */}
+          {movies.map((movie) => (  
+            /* Adding props to MovieCard  */
+          <MovieCard movie ={movie}/>
+         ))}
       </div>
+      ) : (
+        <div className="empty">
+          <h2>No movie found</h2>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 
-const MovieCard = ({movie1}) => {
+const MovieCard = ({movie}) => {
   return (
     <div className="movie">
       <div>
-        <p>{movie1.Year}</p>
+        <p>{movie.Year}</p>
       </div>
 
       <div>
         <img
           src={
-            movie1.Poster !== "N/A"
-              ? movie1.Poster
+            movie.Poster !== "N/A"
+              ? movie.Poster
               : "https://via.placeholder.com/400"
           }
            alt ="img" />
       </div>
       <div>
-        <span>{movie1.Type}</span>
-        <h3>{movie1.Title}</h3>
+        <span>{movie.Type}</span>
+        <h3>{movie.Title}</h3>
       </div>
     </div>
   );
