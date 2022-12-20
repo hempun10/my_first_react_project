0c39c928b47bf222123394842634ed42cb1ff8bb
hem <hemdrpun10@gmail.com>
2022-12-20 21:22:38 +0545

    App.css added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useEffect } from "react";
+// 7b225a20
+const API_URL = 'https://www.omdbapi.com?apikey=7b225a20'
+const App=()=> {
 
-function App() {
+  const searchMovies = async(title)=>{
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data.Search)
+  }
+  useEffect(()=>{
+    searchMovies("Avatar")
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
