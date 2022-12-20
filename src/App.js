2eadf8fefec81930c938982038d1af8fd23d5bdd
hem <hemdrpun10@gmail.com>
2022-12-20 22:11:26 +0545

    MovieCard comoponent created and export as an component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
 import React from "react";
 import { useEffect } from "react";
+import "./App.css";
+import SearchIcon from "./search.svg";
 // 7b225a20
-const API_URL = 'https://www.omdbapi.com?apikey=7b225a20'
-const App=()=> {
+const API_URL = "https://www.omdbapi.com?apikey=7b225a20";
+const App = () => {
+  const movie1 = {
+    Title: "Avatar: The Last Airbender",
+    Year: "2005–2008",
+    imdbID: "tt0417299",
+    Type: "series",
+    Poster: "https://m.media-amazon.com/images/M/MV5BODc5YTBhMTItMjhkNi00ZTIxLWI0YjAtNTZmOTY0YjRlZGQ0XkEyXkFqcGdeQXVyODUwNjEzMzg@._V1_SX300.jpg",
+  };
 
-  const searchMovies = async(title)=>{
+  const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
-    console.log(data.Search)
-  }
-  useEffect(()=>{
-    searchMovies("Avatar")
-  },[])
+    console.log(data.Search);
+  };
+  useEffect(() => {
+    searchMovies("Avatar");
+  }, []);
+
   return (
-    <div className="App">
+    <div className="app">
+      <h1>MovieMandu</h1>
+
+      <div className="search">
+        <input
+          placeholder="Search your fav movies !"
+          value="SuperMan"
+          onChange={() => {}}
+        />
+        <img src={SearchIcon} alt="search-icon" onClick={() => {}} />
+      </div>
+      <div className="container">
+       
+      </div>
     </div>
   );
-}
+};
 
 export default App;
